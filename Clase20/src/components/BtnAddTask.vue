2e9649db368c421b1f9ab7eb35f290137d9f5712
FacundoMamonde <facundo.mamonde.96@gmail.com>
2022-10-03 22:02:59 -0300

    Clase 20

---
+++ Clase20/src/components/BtnAddTask.vue
@@ -0,0 +1,25 @@
+<template>
+    <button @click="addTask()">Add Task</button>
+</template>
+  
+<script>
+
+export default {
+    name: 'BtnAddTask',
+    props: {
+        tasktext:String,
+    },
+    data() {
+    }, 
+    methods:{
+        addTask(){
+            this.$emit("btnAddTask",this.tasktext)
+        }
+    }
+}
+
+</script>
+  
+<style>
+
+</style>
---
+++ Clase20/src/components/Task.vue
@@ -0,0 +1,22 @@
+<template>
+    {{ task.data }}
+
+</template>
+  
+<script>
+
+
+export default {
+    name: 'TaskComponent',
+    props: {
+        task: Object,
+
+    },
+    components: {
+        
+    }
+}
+</script>
+  
+
+  
